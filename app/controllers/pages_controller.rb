318a8908c12ca808dd7a8c4e5d0b815c4838b77a
Ryan MacInnes <ryan.macinnes@gmail.com>
2013-05-23 12:25:14 -0400

    Adding connect page and styling.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def connect
+  end
+
+  def admin
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 B2plus::Application.routes.draw do
 
   resource :team, except: :show
+  get 'connect', to: 'pages#connect'
   get '/team', to: 'teams#index'
   get '/admin', to: 'pages#admin'
   resources :pages
