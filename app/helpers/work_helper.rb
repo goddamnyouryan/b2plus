4fe42ef47821af60d4590c0d5cd491691df764d6
Ryan MacInnes <ryan.macinnes@gmail.com>
2013-06-08 21:11:54 -0400

    Style fixes.

--- app/helpers/work_helper.rb
+++ app/helpers/work_helper.rb
@@ -1,6 +1,3 @@
 module WorkHelper
 
-  def background_images(work)
-    "background-image: url('#{work.foreground_image}'), url('#{work.middle_image}'), url('#{work.background_image}');"
-  end
 end
