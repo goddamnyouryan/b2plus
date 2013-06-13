7af96b6ac6ffc7eed3a4453cf3d69c682de6b088
Ryan MacInnes <ryan.macinnes@gmail.com>
2013-06-13 02:12:54 -0400

    Condolidating controllers.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
+  def index
+    @home = Home.first
+    @work = Work.order('position asc')
+    @teams = Team.all
+  end
+
+  def admin
+    @work = Work.all
+    @team = Team.all
+    @home = Home.first
+  end
+
+  def contact
+    UserMailer.contact_form(params).deliver
+    render nothing: true
+  end
+
   def edit
     @home = Home.find params[:id]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 B2plus::Application.routes.draw do
   resources :team, except: [:show, :index]
   resources :work, except: [:show, :index]
-  resources :home, only: [:edit, :update]
-  get '/admin', to: 'pages#admin'
-  post '/contact', to: 'pages#contact'
-  resources :pages, only: [:index, :admin, :contact]
+  resources :home, only: [:index, :admin, :contact, :edit, :update]
+  get '/admin', to: 'home#admin'
+  post '/contact', to: 'home#contact'
 
-  root to: 'pages#index'
+  root to: 'home#index'
 end
